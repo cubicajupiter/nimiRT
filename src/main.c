5e2309fc57f311523db930d058eeb9a2928192d2
Janne Valkama <jvalkama@c1r1p4.hive.fi>
2026-01-30 09:29:26 +0200

    basic main.c program flow

--- src/main.c
+++ src/main.c
@@ -6,8 +6,43 @@
 /*   By: jvalkama <jvalkama@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 15:55:42 by jvalkama          #+#    #+#             */
-/*   Updated: 2026/01/28 15:56:15 by jvalkama         ###   ########.fr       */
+/*   Updated: 2026/01/30 09:28:18 by jvalkama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "defines.h"
+
+void    instruct(void);
+
+int main(int ac, char **av)
+{
+    if (ac == 2)
+    {
+        parse();    //      -> check & fetch scene
+        initialise();   //  -> wrap up a handy struct(s)
+        trace();    //      -> the BIG LOOP(S) OF MATHS.
+        render();   //      -> MLX images
+        cleanup();  //      -> free allocations / mutexes / threads/ anything else..
+    }
+    else
+        instruct();
+    return (0);
+}
+
+void    instruct(void)
+{
+    printf("\nUsage:\n\t./miniRT <scene description as a .rt file>\n");
+}
+
+void    trace(void)
+{
+    while (x)
+    {
+        while (y)
+        {
+            ray();
+            intersection();
+            shading();
+        }
+    }
+}
