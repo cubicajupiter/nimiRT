ba5a757a357f34c0c2e2f6eea007baa8f646bb56
Janne Valkama <janne.e.valkama@gmail.com>
2026-02-03 11:34:00 +0200

    matrices

--- src/primitives/matrices.c
+++ src/primitives/matrices.c
@@ -6,13 +6,13 @@
 /*   By: jvalkama <jvalkama@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 14:53:19 by jvalkama          #+#    #+#             */
-/*   Updated: 2026/02/02 19:13:49 by jvalkama         ###   ########.fr       */
+/*   Updated: 2026/02/03 11:33:29 by jvalkama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-int matrix_compare(matrix a, matrix b)
+int matrix_compare(t_matrix a, t_matrix b)
 {
     int     i;
     int     j;
@@ -34,7 +34,7 @@ int matrix_compare(matrix a, matrix b)
     return (TRUE);
 }
 
-int matrix_multiply(matrix dst, matrix a, matrix b)
+int matrix_multiply(t_matrix dst, t_matrix a, t_matrix b)
 {
     float       result;
     int         i;
@@ -61,7 +61,7 @@ int matrix_multiply(matrix dst, matrix a, matrix b)
     return (SUCCESS);
 }
 
-int matrix_tuple_multiply(tuple dst, matrix a, tuple b)
+int matrix_tuple_multiply(t_tuple dst, t_matrix a, t_tuple b)
 {
     float       result;
     int         i;
@@ -85,9 +85,9 @@ int matrix_tuple_multiply(tuple dst, matrix a, tuple b)
     return (SUCCESS);
 }
 
-int matrix_transpose(matrix dst, matrix src)
+int matrix_transpose(t_matrix dst, t_matrix src)
 {
-	tuple		row;
+	t_tuple		row;
 	int			i;
 	int			j;
 
--- src/primitives/matrix_invert.c
+++ src/primitives/matrix_invert.c
@@ -6,7 +6,7 @@
 /*   By: jvalkama <jvalkama@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 15:20:21 by jvalkama          #+#    #+#             */
-/*   Updated: 2026/02/02 19:13:46 by jvalkama         ###   ########.fr       */
+/*   Updated: 2026/02/03 11:32:40 by jvalkama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,38 +17,103 @@ Determinants are needed for inverting a matrix.
 Determinants are calculated from 2x2 submatrices.
 2x2 submatrices are made by deleting a row & column from a 4x4 matrix to make a 3x3, then again for a 2x2.
 	(This divide-and-conquer submatrix logic is in the file 'submatrix.c')
-Determinants found in submatrices are called 'minors'.
+The Determinants found in these submatrices are called 'minors'.
+Cofactors are needed to calculate determinants for matrices larger than 2x2.
+	- To calculate the cofactors, you calculate minors from submatrices.
+		-- SO the minors of 4x4 matrices are the determinants of 3x3 matrices,
+		-- and the minors of 3x3 are the determinants of its 2x2 matrices.
+You need a matrix of cofactors, which is then transposed and finally divided by the determinant
+of the original matrix.
 */
+
+//NOTE: Make sure 4x4 -> 3x3 -> 2x2 divide-and-conquer happens correctly!!!!!!!!!!
 
 #include "miniRT.h"
 
-int	matrix_invert()
+static bool		is_invertible(t_matrix m);
+static int		determinant(float *det, float **matrix, int width);
+static float	minor(float **matrix, int row, int column, int width);
+static float	cofactor(float **matrix, int row, int column, int width);
+
+int	matrix_invert(t_matrix dst, t_matrix src)
 {
-	;
+	float	det;
+	int		i;
+	int		j;
+
+	if (!dst || !src)
+		return (ft_error(EINVAL, "matrix_invert"));
+	if (!is_invertible(src))
+		return (1);
+	i = 0;
+	j = 0;
+	det = 0.0;
+	while (i < 4)
+	{
+		while (j < 4)
+		{
+			determinant(&det, src, 4)
+			dst[j][i] = cofactor(src, i, j, 4) / det;
+			j++;
+		}
+		i++;
+	}
+	return (SUCCESS);
 }
 
-static int	determinant(float *det, matrix2 matrix)
+static int	determinant(float *det, float **matrix, int width)
 {
-	if (!det || !matrix)
+	int		column;
+
+	if (!det || !matrix || !width)
 		return (ft_error(EINVAL, "determinant"));
-	*det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+	if (width == 2)
+		*det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+	else if (width > 2)
+	{
+		column = 0;
+		while (column < width)
+		{
+			*det += matrix[0][column] * cofactor(matrix, 0, column, width);
+		}
+	}
 	return (SUCCESS);
 }
 
-static int	minor(float *det, matrix3 matrix, int row, int column)
+static float	minor(float **matrix, int row, int column, int width)
 {
-	if (!det || !matrix || !row || !column)
-		return (ft_error(EINVAL, "minor"));
-	determinant(det, submatrix2(matrix, row, column));
-	return (SUCCESS);
+	float		det;
+
+	if (width == 3)
+		determinant(&det, submatrix2((t_matrix3) matrix, row, column), 2);
+	else if (width == 4)
+		determinant(&det, submatrix3((t_matrix) matrix, row, column), 3);
+	return (det);
 }
 
-static int	cofactor(float *det, matrix3 matrix, int row, int column)
+static float	cofactor(float **matrix, int row, int column, int width)
 {
-	if (!det || !matrix || !row || !column)
-		return (ft_error(EINVAL, "cofactor"));
-	minor(det, matrix, row, column);
+	float		det;
+
+	det = minor(matrix, row, column, width);
 	if (row + column % 2 == 1)
-		*det *= -1;
-	return (SUCCESS);
+		det *= -1;
+	return (det);
+}
+
+static bool	is_invertible(t_matrix matrix)
+{
+	float		det;
+	
+	det = 0.0;
+	determinant(&det, matrix, 4);
+	if (is_float_equal(det, 0.0))
+		return (false);
+	return (true);
+}
+
+
+int	main(void)
+{
+	; //TODO: TESTS AFTER EXAM!!!!!!!!!!
 }
